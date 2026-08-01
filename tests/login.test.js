9879e744d0c86c7acd5afbc07e91d34123aaf5d1
Thiago Folly de Oliveira <folly@MacBook-Pro-de-Thiago.local>
2026-08-01 18:05:15 -0300

    Criei um arquivo de configuração e apliquei variaveis de ambiente

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,5 +1,6 @@
 import http from 'k6/http'
 import { sleep, check } from 'k6'
+import { pegarBaseURL } from '../utils/variaveis.js'
 const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
 
 export const options = {
@@ -15,9 +16,10 @@ export const options = {
 }
 
 export default function () {
-    const url = 'http://localhost:3000/login'
+    const url = `${pegarBaseURL()}/login`
 
-    postLogin.username = 'junio.lima'
+    postLogin.username = 'junior.lima'
+
     const payload = JSON.stringify(postLogin)
 
     const params = {

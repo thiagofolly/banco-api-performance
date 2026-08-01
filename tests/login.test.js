048d93b6cb68abe2fa206f92ace8184aa4528a52
Thiago Folly de Oliveira <folly@MacBook-Pro-de-Thiago.local>
2026-08-01 16:24:47 -0300

    adicionando arquivo inicial com teste de login

--- tests/login.test.js
+++ tests/login.test.js
@@ -4,9 +4,9 @@ import { sleep, check } from 'k6'
 export const options = {
     thresholds: {
         http_req_failed: ['rate<0.01'],
-        http_req_duration: ['p(90)<10', 'max<20']
+        http_req_duration: ['p(90)<10', 'max<15']
     },
-    iterations: 10
+    iterations: 50
 }
 
 export default function () {
